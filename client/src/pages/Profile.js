f89d230f4019b5416fbb269d29cd1ac88cf370e5
Daniel Lucas <danomiterock@yahoo.com>
2021-04-30 12:46:22 -0700

    create boiler plate for profile page

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export const Profile = () => {
+  return (
+    <Container>
+      <div>
+          
+      </div>
+    </Container>
+  );
+};
+export default React;
