7799aa419368e3409d8429a3d42009d5c5ed2540
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-05-01 21:00:31 -0700

    React-added index.css and navbar page

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import {useLocation} from "react-router-dom"
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap'
+import "../index.css"
 
 function Nav() {
-  return (
-    <Navbar bg="primary" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#search">Search</Nav.Link>
-      <Nav.Link href="#about">about</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-light">Search</Button>
-    </Form>
-  </Navbar>
-  );
-}
+    const location = useLocation();
+
+    return (
+        <>
+    <Navbar className="brownBg">
+      <Navbar.Brand href="/"><h1 className="secondaryColor">Campaholic</h1></Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/"><p className="whiteColor">Home</p></Nav.Link>
+        <Nav.Link href="aboutme"><p className="whiteColor">About</p></Nav.Link>
+        <Nav.Link href="contact"><p className="whiteColor">Profile</p></Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button className="primaryColor" variant="outline-info">Search</Button>
+      </Form>
+    </Navbar>
+    <br />
+  </>
+  
+    );
+  }
 
 export default Navbar;
