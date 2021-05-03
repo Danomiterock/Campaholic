34e7b4215b289e3922c2f7cf972e9d75455e15ca
Daniel Lucas <danomiterock@yahoo.com>
2021-05-03 15:22:22 -0700

    fix about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Card, link, Row } from "react-bootstrap";
+import { Container, Card, Row } from "react-bootstrap";
 
 export const About = () => {
   return (
@@ -7,67 +7,67 @@ export const About = () => {
       <Row>
         <div>
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="../public/assets/image/myrla.jpg" />
+            <Card.Img variant="top" src="../assets/image/myrla.jpg" />
             <Card.Body>
               <Card.Title>Myrla Flores</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
-              </Card.Text>
-              <link
+              <a
                 variant="primary"
                 href="https://competent-kepler-c24de2.netlify.app/"
               >
                 View Portfolio
-              </link>
+              </a>
+              </Card.Text>
             </Card.Body>
           </Card>
         </div>
         <div>
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="../public/assets/image/profpic.jpg" />
+            <Card.Img variant="top" src="../assets/image/profpic.jpg" />
             <Card.Body>
               <Card.Title>Triston Burns</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
-              </Card.Text>
-              <link
+              <a
                 variant="primary"
                 href="https://tcb-react-portfolio.herokuapp.com/"
               >
                 View Portfolio
-              </link>
+              </a>
+              </Card.Text>
             </Card.Body>
           </Card>
         </div>
         <div>
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="../pub;ic/assets/image/RonPic.jpg" />
+            <Card.Img variant="top" src="../assets/image/RonPic.jpg" />
             <Card.Body>
               <Card.Title>Wes Howard</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
-              </Card.Text>
-              <link variant="primary" href="">
+              <a variant="primary" href="">
                 View Portfolio
-              </link>
+              </a>
+              </Card.Text>
             </Card.Body>
           </Card>
         </div>
         <div>
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="../public/assets/image/Portfolio.js" />
+            <Card.Img variant="top" src="../assets/image/Portrait.jpg" />
             <Card.Body>
               <Card.Title>Dan Lucas</Card.Title>
               <Card.Text>
                 Dan is a former police officer with a strong background in
                 communication.
-              </Card.Text>
-              <link variant="primary" href="https://danomiterock.netlify.app/">
+              <a variant="primary" href="https://danomiterock.netlify.app/">
                 View Portfolio
-              </link>
+              </a>
+              </Card.Text>
             </Card.Body>
           </Card>
         </div>
