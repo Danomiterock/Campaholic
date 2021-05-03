f6a9ea93fac2e0c78de76ffcee9c137650931248
Daniel Lucas <danomiterock@yahoo.com>
2021-05-03 10:59:46 -0700

    remove extra navlist

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,10 +4,9 @@ import NavList from "../NavList";
 export function Header() {
   return (
     <header className="masthead">
-        <div className="headnav">
-            <h1> Campaholic </h1>
-            <NavList />
-        </div>
+      <div className="headnav">
+        <h1> Campaholic </h1>
+      </div>
     </header>
   );
 }
