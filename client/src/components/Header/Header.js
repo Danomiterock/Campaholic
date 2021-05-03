0d68cfc092b3cb6928836d56b3da319e46bd5d9d
Daniel Lucas <danomiterock@yahoo.com>
2021-05-03 14:57:18 -0700

    Signup page working

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import LoginSignup from "../../pages/LoginSignup";
-import "../../index.css";
+import "../../index.css"
+
 
 export function Header() {
   return (
     <header className="masthead">
-      <div className="headnav">
-        <h1> Campaholic </h1>
-      </div>
-      <Button exact path="/LoginSignup" component={LoginSignup}>
-        <p className="loginSignup">SignIn</p>
-      </Button>
-      <Button exact path="/LoginSignup" component={LoginSignup}>
-        <p className="loginSignup">SignUp</p>
-      </Button>
+        <div className="headnav">
+            <h1> Campaholic </h1>            
+        </div>
+        <Button href="/LoginSignup"><p className="loginSignup">SignIn</p></Button>
+        <Button href="/LoginSignup"><p className="loginSignup">SignUp</p></Button>
     </header>
   );
 }
